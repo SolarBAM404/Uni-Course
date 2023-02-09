292753dcb79a9e9251a8d02d56e98ad16981fb63
Solar Uni <100665218@unimail.derby.ac.uk>
2023-02-09 17:42:41 +0000

    :sparkles: added basic week switcher

--- Semester 2/Programming 2/ReplitUnits/Program.cs
+++ Semester 2/Programming 2/ReplitUnits/Program.cs
@@ -1,3 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ReplitUnits.Week1;
+
+void ToConsole(String msg)
+{
+    Console.WriteLine(msg);
+}
+
+ToConsole("Enter a Week (1-4)");
+int weekNum = Int32.Parse(Console.ReadLine());
+
+ToConsole("Enter number of the unit");
+int unitNum = Int32.Parse(Console.ReadLine());
+
+switch (weekNum)
+{
+    case (1):
+        switch (unitNum)
+        {
+            case (1) :
+                HelloCS.Main_hellocs();
+                break;
+            case (2):
+                Objects_Etc.Main_objects();
+                break;
+            case(3):
+                Comments.Main_comments();
+                break;
+            case (4):
+                Errors.Main_errors();
+                break;
+            case (5):
+                Style.Main_style();
+                break;
+            case (6):
+                Input.Main_input();
+                break;
+            case (7):
+                SimpleIO.Main_simpleIO();
+                break;
+            case (8):
+                Ex1.Main_ex1();
+                break;
+            case (9):
+                Ex2.Main_ex2();
+                break;
+            default:
+                ToConsole("Not an available unit!");
+                break;
+        }
+        break;
+    
+    default:
+        ToConsole("Not an available Week!");
+        break;
+}
